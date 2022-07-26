a8b5e4c7565f005a376d37e5501bdeac0f34b4af
AntonMuravlev <antonforward@gmail.com>
2022-07-26 17:49:57 +0300

    Chapter 01 is completed

--- exercises/01_pytest_basics/task_1_6.py
+++ exercises/01_pytest_basics/task_1_6.py
@@ -17,3 +17,44 @@
 
 Для заданий этого раздела нет тестов для проверки тестов :)
 """
+
+
+import pytest
+import yaml
+from pprint import pprint
+from scrapli import Scrapli
+
+
+with open("devices_task5.yaml") as f:
+    all_devices = yaml.safe_load(f)
+
+with open("net_interfaces_up.yaml") as f:
+    devices_and_intf = yaml.safe_load(f)
+
+ip_list = list(devices_and_intf.keys())
+
+check_devices = []
+
+for device in all_devices:
+    if device["host"] in ip_list:
+        check_devices.append(device)
+
+
+@pytest.fixture(scope="session", params=check_devices, ids=ip_list)
+def ssh_connection(request):
+    ssh = Scrapli(**request.param)
+    ssh.open()
+    yield ssh
+    ssh.close()
+
+
+def test_ip_intf(ssh_connection):
+    intf_list = devices_and_intf[ssh_connection.host]
+    parsed_out = ssh_connection.send_command("sh ip int br").textfsm_parse_output()
+    real_int_list = [real_int["intf"] for real_int in parsed_out]
+    real_int_status = {}
+    assert set(intf_list).issubset(set(real_int_list)), "Router doesn't have all interfaces from net_interfaces_up.yaml"
+    for real_int in parsed_out:
+        real_int_status[real_int["intf"]] = real_int["proto"]
+    for intf in intf_list:
+        assert real_int_status[intf] == "up"
